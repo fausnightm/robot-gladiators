db634970b3434ce3b3ee1dcd1f5faedb184f30f1
Fausnightm <m.fausnight@lafilm.edu>
2021-06-05 21:21:55 -0400

    high score feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -132,47 +132,32 @@ var startGame = function() {
 };
 
 var endGame = function() {
-  if (playerInfo.health > 0) {
-    window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+  window.alert("The game has now ended. Let's see how you did!");
+
+  // check localStorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+    highScore = 0;
   }
+  // if player has more money than the high score, player has new high score!
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+  } 
   else {
-    window.alert("You've lost your robot in battle.")
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
   }
+
+  // ask player if they'd like to play again
   var playAgainConfirm = window.confirm("Would you like to play again?");
 
   if (playAgainConfirm) {
     startGame();
-  }
+  } 
   else {
     window.alert("Thank you for playing Robot Gladiators! Come back soon!");
-  }
-};
-
-var shop = function() {
-  var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, and 3 for LEAVE."
-  );
-
-  shopOptionPrompt = parseInt.shopOptionPrompt
-
-  switch (shopOptionPrompt) {
-    case 1:
-        playerInfo.refillHealth();
-        break;
-
-    case 2:
-        playerInfo.upgradeAttack();
-        break;
-
-    case 3:
-      window.alert("leaving the store.");
-      break;
-
-    default:
-      window.alert("You did not pick a valid option. Try again.");
-
-      shop();
-      break;
   }
 };
 
